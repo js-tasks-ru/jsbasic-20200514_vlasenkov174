c980273f029f522dde90487ffa080a658d5edc28
Bogdan Vlasenkov <vlasenkov174@mail.ru>
2020-05-28 22:52:05 +0500

    убрал все лишние знаки, сделал из строки массив, сделал все элементы числами, нашел максимальное и минимальное число

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -3,5 +3,16 @@
  * @param   {string} str -  входные данные
  * @returns {{min:number, max:number}}  объект
  */
+
 function getMinMax(str) {
+let arr = str
+    .split(' ')
+    .join(',')
+    .split(',')
+    .map(val=>parseFloat(val))
+    .filter(val=>!Number.isNaN(val));
+  return {
+    min: Math.min(...arr),
+    max: Math.max(...arr)
+  };
 }
