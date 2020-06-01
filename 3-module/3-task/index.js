38bd9b8fd71525e52f4212a22d8e40b8987faf90
Bogdan Vlasenkov <vlasenkov174@mail.ru>
2020-06-01 21:11:51 +0500

    использовал split и join, чтобы менять текст

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -3,4 +3,18 @@
  * @returns {string}
  */
 function camelize(str) {
+    let splitStr = str.split('-');
+    let mapStr = splitStr.map(function(word, index) {
+        if (index == 0) {
+            return word
+        }
+        else {
+            return word[0].toUpperCase() + word.slice(1)
+        }
+        
+    });
+    console.log(mapStr)
+    let joinStr = mapStr.join('');
+    return joinStr;
 }
+camelize("background-color") == 'backgroundColor';
